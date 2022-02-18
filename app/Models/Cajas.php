8d3c50c3deb40444570967fb92cea315ae808c36
chagy <chagy_x_@hotmail.com>
2022-02-18 08:34:57 +0700

    15

---
+++ app/Models/Cajas.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cajas extends Model
+{
+    use HasFactory;
+
+    protected $table = "cajas";
+
+    protected $fillable = [
+        'monto',
+        'tipo',
+        'concepto',
+        'comprobante',
+        'user_id'
+    ];
+}
