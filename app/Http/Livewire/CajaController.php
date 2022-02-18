0273f1925c8fc8e80281de30607181ac2ce2715e
chagy <chagy_x_@hotmail.com>
2022-02-18 09:40:30 +0700

    17

--- app/Http/Livewire/CajaController.php
+++ app/Http/Livewire/CajaController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Caja;
 use Livewire\Component;
 use Livewire\WithPagination;
-use App\Models\Caja;
+use Illuminate\Support\Facades\Auth;
 
 class CajaController extends Component
 {
@@ -64,5 +65,83 @@ class CajaController extends Component
         $this->monto = $record->monto;
         $this->comprobante = $record->comprobante;
         $this->action = $record->action;
+    }
+
+    public function StoreOrUpdate()
+    {
+        $this->validate([
+            'tipo' => 'not_in:Elegir'
+        ]);
+
+        $this->validate([
+            'tipo' => 'required',
+            'monto' => 'required',
+            'concepto' => 'required'
+        ]);
+
+        if($this->selected_id <= 0){
+            $caja = Caja::create([
+                'monto' => $this->monto,
+                'tipo' => $this->tipo,
+                'concepto' => $this->concepto,
+                'user_id' => Auth::user()->id
+            ]);
+
+            if($this->comprobante){
+                $image = $this->comprobante;
+                $fileName = time().'.'.explode('/',explode(':',substr($image,0,strpos($image,';')))[1])[1];
+                $moved = \Image::make($image)->save('images/'.$fileName);
+
+                if($moved){
+                    $caja->comprobante = $fileName;
+                    $caja->save();
+                }
+            }
+        }else{
+            $record = Caja::find($this->selected_id);
+            $record = Caja::update([
+                'monto' => $this->monto,
+                'tipo' => $this->tipo,
+                'concepto' => $this->concepto,
+                'user_id' => Auth::user()->id
+            ]);
+            if($this->comprobante){
+                $image = $this->comprobante;
+                $fileName = time().'.'.explode('/',explode(':',substr($image,0,strpos($image,';')))[1])[1];
+                $moved = \Image::make($image)->save('images/'.$fileName);
+
+                if($moved){
+                    $record->comprobante = $fileName;
+                    $record->save();
+                }
+            }
+        }
+
+        if($this->selected_id){
+            $this->emit('msgok','Movimiento de Caja Actualizado con Exito');
+        }else{
+            $this->emit('msgok','Movimiento de Caja fue creado con Exito');
+        }
+
+        $this->resetInput();
+    }
+
+    protected $listeners = [
+        'deleteRow' => 'destroy',
+        'fileUpload' => 'handleFileUpload'
+    ];
+
+    public function handleFileUpload()
+    {
+        $this->comprobante = $imageData;
+    }
+
+    public function destroy($id)
+    {
+        if($id){
+            $record = Caja::where('id',$id);
+            $record->delete();
+            $this->resetInput();
+        }
     }
 }
