7d52bc9bdfea6641a9ea1615e609e7e64f1263fb
chagy <chagy_x_@hotmail.com>
2022-02-21 13:14:22 +0700

    24

---
+++ app/Models/Vehiculo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+    use HasFactory;
+
+    protected $table = "vehiculos";
+
+    protected $fillable = [
+        'placa',
+        'modelo',
+        'marca',
+        'color',
+        'tipo_id',
+    ];
+}
