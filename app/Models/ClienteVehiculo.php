61120964f3ec19eca9823c8626c09d0d4a53d6bb
chagy <chagy_x_@hotmail.com>
2022-02-22 09:27:49 +0700

    35

---
+++ app/Models/ClienteVehiculo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClienteVehiculo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cliente_vehiculos';
+
+    protected $fillable = [
+        'user_id',
+        'vehiculo_id'
+    ];
+}
--- app/Models/Renta.php
+++ app/Models/Renta.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Renta extends Model
 {
     use HasFactory;
+
+    protected $table = 'rentas';
+
+    protected $fillable = [
+        'acceso',
+        'hours',
+        'salida',
+        'placa',
+        'modelo',
+        'marca',
+        'color',
+        'llaves',
+        'total',
+        'efectivo',
+        'cambio',
+        'user_id',
+        'vehiculo_id',
+        'tarifa_id',
+        'barcode',
+        'estatus',
+        'description',
+    ];
 }
